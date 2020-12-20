ae5b43bd1e7e7f1add0b5286d89645e6db402930
Sergey Stukach <sergeyst80@gmail.com>
2020-12-20 11:41:11 +0300

    Home work #03. Use round function

--- sergey_stukach.py
+++ sergey_stukach.py
@@ -3,12 +3,11 @@ param = {'weight': 0, 'height': 0}
 param['weight'] = int(input("Input your weight (kg): "))
 param['height'] = int(input("Input your height (cm): "))
 bmi = param['weight'] / ((param['height'] / 100) ** 2)
-
 print(f"Your BMI (body mass index): {bmi:.2f} kg/m2")
 
 max_value = 70
 min_value = 10
-before = int(bmi) - min_value
-after = max_value - int(bmi)
+before = round(bmi) - min_value
+after = max_value - round(bmi)
 
-print(str(min_value) + '=' * before + '|' + '='*after + str(max_value))
+print(str(min_value) + '=' * before + '|' + '=' * after + str(max_value))
