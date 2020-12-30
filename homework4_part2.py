95c7b98d482bbc82e6550fa58acdef9729078541
SergeySt <sergeyst80@gmail.com>
2020-12-30 13:23:44 +0300

    Homework 5 update

--- homework4_part2.py
+++ homework4_part2.py
@@ -7,13 +7,13 @@ clear_board = """
 ---+---+---
  7 | 8 | 9 
 """
-clr_str = ["clear","cls"][os.name=="nt"]
+clr_str = ["clear", "cls"][os.name == "nt"]
 symbols = ["O", "X"]
 current_player = False
 game_repeat = True
 
 while game_repeat:
-    board = {'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9}
+    board = {'1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
     os.system(clr_str)
 
     while True:
@@ -30,32 +30,32 @@ while game_repeat:
 
         while True:
             position = input("Введите номер ячейки: ")
-            if len(position) == 1 and position > "0" and position <= "9":
+            if len(position) == 1 and "0" < position <= "9":
                 if type(board[position]) == int:
                     board[position] = symbols[current_player]
                     break
 
         os.system(clr_str)
         print("\n {} | {} | {} \n".format(board['1'], board['2'], board['3'])
-            +"---+---+---\n"
-            +" {} | {} | {} \n".format(board['4'], board['5'], board['6'])
-            +"---+---+---\n"
-            +" {} | {} | {} \n".format(board['7'], board['8'], board['9']))
+              + "---+---+---\n"
+              + " {} | {} | {} \n".format(board['4'], board['5'], board['6'])
+              + "---+---+---\n"
+              + " {} | {} | {} \n".format(board['7'], board['8'], board['9']))
 
-        if (board['1']==board['2']==board['3']
-         or board['4']==board['5']==board['6']
-         or board['7']==board['8']==board['9']
-         or board['1']==board['4']==board['7']
-         or board['2']==board['5']==board['8']
-         or board['3']==board['6']==board['9']
-         or board['1']==board['5']==board['9']
-         or board['7']==board['5']==board['3']):
+        if (board['1'] == board['2'] == board['3']
+                or board['4'] == board['5'] == board['6']
+                or board['7'] == board['8'] == board['9']
+                or board['1'] == board['4'] == board['7']
+                or board['2'] == board['5'] == board['8']
+                or board['3'] == board['6'] == board['9']
+                or board['1'] == board['5'] == board['9']
+                or board['7'] == board['5'] == board['3']):
             print("Ура!!! Победил игрок \"{}\"!!!".format(symbols[current_player]))
             break
         if step == 8:
             print("Ничья!")
 
         current_player = not current_player
-    
+
     if not input("Начать новую игру? (\"Y\" - начать, любой другой символ - выход): ").upper() == "Y":
         game_repeat = False
--- homework5.py
+++ homework5.py
@@ -1,14 +1,15 @@
 import os
 
 cls_str = 'cls' if os.name == 'nt' else 'clear'
-users = {'Alex': '111', 'Nike': '222', 'Mike': '333', 'User': 'User', 'Admin': 'Admin'}
+users = {'alex': '111', 'nike': '222', 'mike': '333', 'user': 'user', 'admin': 'admin'}
 current_user = ''
 user_password = ''
 
 
 def authorization(func):
     def wrapper(*args, **kwargs):
-        check = users.get(input('Enter user name: '))
+        check = users.get(input('Enter user name: ').lower())
+
         if check is None:
             print('User name is unknown! Access denied!')
         elif check == input('Enter password: '):
@@ -36,17 +37,20 @@ def func3():
 while True:
     os.system(cls_str)
     item = input('Functions menu\n====================\n'
-                 + '1 - Function 1 (authorization)\n'
-                 + '2 - Function 2\n'
-                 + '3 - Function 3 (authorization)\n'
-                 + '0 - Exit\n'
+                 + '1\t- Function 1 (authorization)\n'
+                 + '2\t- Function 2\n'
+                 + '3\t- Function 3 (authorization)\n'
+                 + 'Other\t- Exit\n'
+                 + '====================\n'
                  + 'Your choice: ')
-    if item == '0':
-        break
-    elif item == '1':
+
+    if item == '1':
         func1()
     elif item == '2':
         func2()
     elif item == '3':
         func3()
+    else:
+        break
+
     input('Press any key...')
